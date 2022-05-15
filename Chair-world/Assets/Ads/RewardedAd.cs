c1e71a5e27f4186da5e756a3fb8284e9295fd90c
Anechaev06 <57583912+Anechaev06@users.noreply.github.com>
2022-05-15 16:09:02 +0300

    Chair-World

---
+++ Chair-world/Assets/Ads/RewardedAd.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+using TMPro;
+ 
+public class RewardedAd : MonoBehaviour, IUnityAdsLoadListener, IUnityAdsShowListener
+{
+    [SerializeField] Button _showAdButton;
+    [SerializeField] string _androidAdUnitId = "Rewarded_Android";
+    [SerializeField] string _iOSAdUnitId = "Rewarded_iOS";
+    string _adUnitId = null; // This will remain null for unsupported platforms
+
+    private int coins = 0;
+    public TMP_Text coinText;
+
+    void Awake()
+    {   
+        #if UNITY_IOS
+                _adUnitId = _iOSAdUnitId;
+        #elif UNITY_ANDROID
+                _adUnitId = _androidAdUnitId;
+        #endif
+    }
+
+    private void Start() 
+    {
+        LoadAd();
+    }
+
+    public void LoadAd()
+    {
+        Debug.Log("Loading Ad: " + _adUnitId);
+        Advertisement.Load(_adUnitId, this);
+    }
+ 
+    public void OnUnityAdsAdLoaded(string adUnitId)
+    {
+        Debug.Log("Ad Loaded: " + adUnitId);
+ 
+        if (adUnitId.Equals(_adUnitId))
+        {
+            _showAdButton.onClick.AddListener(ShowAd);
+        }
+    }
+ 
+    public void ShowAd()
+    {
+        Advertisement.Show(_adUnitId, this);
+        Advertisement.Load(_adUnitId, this);
+    }
+ 
+    public void OnUnityAdsShowComplete(string adUnitId, UnityAdsShowCompletionState showCompletionState)
+    {
+        if (adUnitId.Equals(_adUnitId) && showCompletionState.Equals(UnityAdsShowCompletionState.COMPLETED))
+        {
+            // Grant a reward.
+            coins = PlayerPrefs.GetInt("Coins");
+            coins += 25;
+            PlayerPrefs.SetInt("Coins", coins);
+
+            coinText.text = coins.ToString();
+
+            // Load another ad:
+            Advertisement.Load(_adUnitId, this);
+        }
+    }
+ 
+    public void OnUnityAdsFailedToLoad(string adUnitId, UnityAdsLoadError error, string message)
+    {
+        Debug.Log($"Error loading Ad Unit {adUnitId}: {error.ToString()} - {message}");
+    }
+ 
+    public void OnUnityAdsShowFailure(string adUnitId, UnityAdsShowError error, string message)
+    {
+        Debug.Log($"Error showing Ad Unit {adUnitId}: {error.ToString()} - {message}");
+    }
+ 
+    public void OnUnityAdsShowStart(string adUnitId) { }
+    public void OnUnityAdsShowClick(string adUnitId) { }
+ 
+    void OnDestroy()
+    {
+        _showAdButton.onClick.RemoveAllListeners();
+    }
+}
